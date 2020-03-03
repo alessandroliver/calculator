ce62d53fdb54c5e5a84ec2efcd514e0a9b0b775d
alessandroliver <alessandrorodrgues270@gmail.com>
2020-03-03 19:37:33 -0300

    First Commit

---
+++ src/components/Button.vue
@@ -0,0 +1,54 @@
+<template>
+    <button class="button" @click="$emit('onClick', label)"
+        :class="{ double, triple, operation }">
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+    props: { //definir as propriedades, os parâmetros que quero passar pro botão
+        label: {},
+        operation: { type: Boolean },
+        double: { type: Boolean }, //se passar o parâmetro é verdadeiro
+        triple: { type: Boolean }
+    }
+}
+</script>
+
+<style>
+:root {
+    --bg-button: #f0f0f0;
+    --border-button: solid 1px #888;
+}
+
+.button {
+    font-size: 1.4em;
+    background-color: var(--bg-button);
+    border: none;
+    border-right: var(--border-button);
+    border-bottom: var(--border-button);
+    outline: none;
+}
+
+.button:active {
+    background-color: #ccc;
+}
+
+.button.double {
+    grid-column: span 2;
+}
+
+.button.triple {
+    grid-column: span 3;
+}
+
+.button.operation {
+    background-color: #fa8231;
+    color: #fff;
+}
+
+.button.operation:active {
+    background-color: #fa8231cc;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+    render: h => h(App) //criando um elemento
+}).$mount("#app") //e jogando dentro da div, que está dentro do index.html
